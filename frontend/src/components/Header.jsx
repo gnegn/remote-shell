ecee111a7b882aafa1ec039f383bd566de70b487
gnegn <gnegenngne@gmail.com>
2025-09-07 17:02:39 +0000

    deploy
    multi stage build
    
    build and run ec2 image
    
    corrected config for docker local network
    
    network config for console
    
    multistage build 2
    
    removed comments

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import Nav from './Nav';
-<<<<<<< HEAD
 import Logout from './Logout';
 import Brand from './Brand';
-=======
->>>>>>> 20e4b27f4010c94957c544768fdf9f85ba0fe35e
 
 export default function Header() {
   return (
     <header className="header">
-<<<<<<< HEAD
       <Brand />
       <Nav />
       <Logout />
     </header>
   );
-} 
-=======
-      <Nav />
-    </header>
-  );
 }
->>>>>>> 20e4b27f4010c94957c544768fdf9f85ba0fe35e

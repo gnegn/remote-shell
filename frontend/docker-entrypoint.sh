67add5f8c1ad33503693d68a6b4c8728edd5f6fc
gnegn <gnegenngne@gmail.com>
2025-09-16 12:40:08 +0000

    yaml improvements for certbot

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
 
-DOMAIN="remoteshellsoftcom.duckdns.org"
-EMAIL="gnegenngne@gmail.com"
-WEBROOT="/var/www/certbot"
+#DOMAIN="remoteshellsoftcom.duckdns.org"
+#EMAIL="gnegenngne@gmail.com"
+#WEBROOT="/var/www/certbot"
 
 # Перевіряємо, чи сертифікат існує
-if [ ! -f "/etc/letsencrypt/live/$DOMAIN/fullchain.pem" ]; then
-  echo "Сертифікат не знайдено. Генеруємо новий..."
-  certbot certonly --webroot -w $WEBROOT \
-    --email $EMAIL --agree-tos --no-eff-email \
-    -d $DOMAIN -d www.$DOMAIN
-fi
+#if [ ! -f "/etc/letsencrypt/live/$DOMAIN/fullchain.pem" ]; then
+#  echo "Сертифікат не знайдено. Генеруємо новий..."
+#  certbot certonly --webroot -w $WEBROOT \
+#    --email $EMAIL --agree-tos --no-eff-email \
+#    -d $DOMAIN -d www.$DOMAIN
+#fi
 
 # Запускаємо Nginx в foreground
 nginx -g "daemon off;"
